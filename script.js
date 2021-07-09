b799c1fa85d673e6dbdf01d641cf5827747b290e
Tri <triphan921@gmail.com>
2021-07-08 18:20:35 -0700

    html, style and js

---
+++ script.js
@@ -0,0 +1,82 @@
+const X_CLASS = "x";
+const CIRCLE_CLASS = "circle";
+const WINS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+const cellElements = document.querySelectorAll("[cell]");
+const board = document.getElementById("board");
+const winningMessageElement = document.getElementById("winningMessage");
+const winningMessageTextElement = document.querySelector(
+  "[winning-message-text]"
+);
+const restartButton = document.getElementById('restartButton')
+let circleTurn;
+
+const placeMark = (cell, currentClass) => {
+  cell.classList.add(currentClass);
+};
+
+const swapTurns = () => {
+  circleTurn = !circleTurn;
+};
+
+const setBoardHoverClass = () => {
+  board.classList.remove(X_CLASS);
+  board.classList.remove(CIRCLE_CLASS);
+  circleTurn ? board.classList.add(CIRCLE_CLASS) : board.classList.add(X_CLASS);
+};
+
+const checkWin = (currentClass) => {
+  return WINS.some((comb) => {
+    return comb.every((index) => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
+};
+const isDraw = () => {
+    return [...cellElements].every(cell=> {
+        return cell.classList.contains(X_CLASS) ||
+        cell.classList.contains(CIRCLE_CLASS)
+    })
+}
+
+const endGame = (draw) => {
+  // draw? "hi" : winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} Wins!`;
+  if (draw) {
+      winningMessageTextElement.innerText = 'Draw!'
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} Wins!`;
+  }
+  winningMessageElement.classList.add("show");
+};
+
+const handleClick = (e) => {
+  const cell = e.target;
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    swapTurns();
+    setBoardHoverClass();
+  }
+};
+
+const startGame = () => {
+  circleTurn = false;
+  cellElements.forEach((cell) => {
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  setBoardHoverClass();
+};
+
+startGame();
